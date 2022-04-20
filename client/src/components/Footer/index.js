865856936195dfdf4f3bc0db0dbbe3ca5582706e
jpc0023 <jpc0023@uah.edu>
2022-04-20 10:27:48 -0500

    paused

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary2 p-4">
+      <div className="container">&copy;2022 by James Crowell</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-secondary2 mb-4 py-2 flex-row align-center">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <div className='relative'>
+        <h1 >Deep Thoughts </h1> 
+        <div className='h5absolute'><h5>(but not by Jack Handy)</h5></div></div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
