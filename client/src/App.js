e56d9c96803ff2c150c357ff48127c9bdcc318e7
jpc0023 <jpc0023@uah.edu>
2022-05-01 01:30:38 -0500

    profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,14 @@ import {
   createHttpLink,
 } from '@apollo/client';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import NoMatch from './pages/NoMatch';
+import SingleThought from './pages/SingleThought';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -21,17 +29,45 @@ const client = new ApolloClient({
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className="flex-column justify-flex-start min-100-vh">
-        <Header />
-        <div className="container">
-          <Home />
-        </div>
-        <Footer />
-      </div>
-    </ApolloProvider>
-  );
-}
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div className="flex-column justify-flex-start min-100-vh">
+            <Header />
+            <div className="container">
+                <Routes>
+                    <Route
+                    path="/"
+                    element={<Home />}
+                    />
+                    <Route
+                    path="/login"
+                    element={<Login />}
+                    />
+                    <Route
+                    path="/signup"
+                    element={<Signup />}
+                    />
+                    <Route
+                    path="/profile/:username"
+                    element={<Profile />}
+                    />
+                    <Route
+                    path="/thought/:id"
+                    element={<SingleThought />}
+                    />
+
+                    <Route
+                    path="*"
+                    element={<NoMatch />}
+                    />
+                </Routes>
+                </div>
+            <Footer />
+          </div>
+        </Router>
+      </ApolloProvider>
+    );
+  }
 
 export default App;
